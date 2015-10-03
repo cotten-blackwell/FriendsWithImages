76eb37f955f16a11f30943b7253d3cf7a9591f65
cotten-blackwell <cotten.blackwell@gmail.com>
2015-10-03 11:13:34 -0700

    Initial Commit

---
+++ FriendsWithImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendsWithImages
+//
+//  Created by Cotten Blackwell on 10/3/15.
+//  Copyright © 2015 Cotten Blackwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendsWithImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendsWithImages
+//
+//  Created by Cotten Blackwell on 10/3/15.
+//  Copyright © 2015 Cotten Blackwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
